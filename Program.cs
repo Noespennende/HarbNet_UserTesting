87e513831d9571617ab953160ae2bb8b2444536d
Andreasberg89 <andber9@gmail.com>
2024-02-21 15:04:30 +0100

    Initielt oppsett

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using HarbFramework;
+using harbNet;
+
+namespace Harbnet_brukertesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship ship = new Ship("Den sorte Dame", ShipSize.Large, DateTime.Now, true, 6, 10);
+        }
+    }
+}
